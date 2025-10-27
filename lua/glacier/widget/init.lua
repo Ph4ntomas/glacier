536e4184a46d17b1f887b0840c51f3422bad1130
phantomas <phantomas@phantomas.xyz>
2025-10-27 09:30:18 +0100

    glacier.widget: Add textbox to widget module

--- lua/glacier/widget/init.lua
+++ lua/glacier/widget/init.lua
@@ -5,4 +5,5 @@ return {
     prompt = require("glacier.widget.prompt"),
     taglist = require("glacier.widget.taglist"),
     clock = require("glacier.widget.clock"),
+    textbox = require("glacier.widget.textbox"),
 }
