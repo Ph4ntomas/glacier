8d39a9bc69d1299aa16c2214626e7a0ee29c4045
phantomas <phantomas@phantomas.xyz>
2025-11-08 14:33:03 +0100

    rust: Document KeyGrabber

--- rust/glacier/src/keygrabber.rs
+++ rust/glacier/src/keygrabber.rs
@@ -1,3 +1,50 @@
+//! Glacier's input grabbing.
+//!
+//! This module contains building block to grab all keyboard input (except bound keys).
+//!
+//! # KeyGrabber
+//! [`KeyGrabber`]s are object used to capture all keyboard inputs, passing them to callbacks for
+//! processing. This can be used to implements complex interactions that might not be feasible
+//! using key-binding and binding layers. As an example, it's used as the building block for the
+//! [`Modal`] type, which implement VI-like key-bindings.
+//!
+//! # Start and Stop Events
+//!
+//! Callback for these events are called after the [`KeyGrabber`] start grabbing input, and after
+//! it has stopped respectively. The [`on_start`] event is meant to notify calling code that input
+//! grabbing has started, while the [`on_stop`] event can additionally be used to trigger other
+//! [`KeyGrabber`], or to restore some state.
+//!
+//! # Key Events
+//!
+//! Key Events comes in three flavor: [`on_key_press`], [`on_key_release`] and the raw [`on_key_event`].
+//!
+//! The first two are simpler to used, since the key_event is processed ahead of time, and should
+//! be preferred. By default said processing involves ignoring events if they are flagged as
+//! captures, but this behavior can be changed by setting the [`KeyGrabber::ignore_capture`] flag.
+//!
+//! [`on_key_event`] callback are giver the raw unprocessed event, and should perform their own
+//! processing.
+//!
+//! When a key_event is received, all matching callback are called, with `on_key_press` or
+//! `on_key_release` being fired first, followed by `on_key_event`.
+//!
+//! # KeyGrabber Handles
+//!
+//! A [`Handle`] is an owning pointer to a [`KeyGrabber`] state, and function similarly, except it
+//! cannot be used to change the callbacks, and is safe to pass around to other function. Being an
+//! owning type, the lifetime of the [`KeyGrabber`] will be extended as log as at least one
+//! [`Handle`] exists.
+//!
+//! [`WeakHandle`]s are simply non-owning version of [`Handle`]s.
+//!
+//! [`Modal`]: crate::modal::Modal
+//! [`on_start`]: KeyGrabber::on_start
+//! [`on_stop`]: KeyGrabber::on_stop
+//! [`on_key_press`]: KeyGrabber::on_key_press
+//! [`on_key_release`]: KeyGrabber::on_key_release
+//! [`on_key_event`]: KeyGrabber::on_key_event
+
 use std::sync::{Arc, Mutex, Weak};
 
 use xkbcommon::xkb::Keysym;
@@ -15,10 +62,10 @@ type KeyEventCallback = Box<dyn FnMut(&Handle, KeyEvent) + Send + Sync + 'static
 type StartCallback = Arc<dyn Fn(&Handle) + Send + Sync + 'static>;
 type StopCallback = Arc<dyn Fn(&Handle) + Send + Sync + 'static>;
 
-pub struct KeyGrabberProgram;
+struct KeyGrabberProgram;
 
 #[derive(Default)]
-pub struct KeyCallbackData {
+struct KeyCallbackData {
     ignore_capture: bool,
     on_key_press: Option<KeyPressCallback>,
     on_key_release: Option<KeyReleaseCallback>,
@@ -26,55 +73,79 @@ pub struct KeyCallbackData {
 }
 
 #[derive(Default)]
-pub struct Inner {
+struct Inner {
     handle: Option<LayerHandle<()>>,
     callback_data: Arc<Mutex<KeyCallbackData>>,
     on_start: Option<StartCallback>,
     on_stop: Option<StopCallback>,
 }
 
+/// Handle on the [`KeyGrabber`] state.
 pub struct Handle(Arc<Mutex<Inner>>);
 
+/// Non-owning version of a [`Handle`]
 #[derive(Clone)]
 pub struct WeakHandle(Weak<Mutex<Inner>>);
 
+/// Keyboard grabbing object.
+///
+/// See module level [documentation].
+///
+/// [documentation]: self
 pub struct KeyGrabber {
     handle: Handle,
 }
 
 impl KeyGrabber {
+    /// Create a new [`KeyGrabber`]
     pub fn new() -> Self {
         Self::default()
     }
 
+    /// Start grabbing input.
+    ///
+    /// Return a [`Handle`] to the [`KeyGrabber`] state.
     pub fn start(&self) -> Handle {
         self.handle.start();
 
-        Handle(self.handle.0.clone())
+        self.handle.clone()
     }
 
+    /// Stop grabbing input.
     pub fn stop(&self) {
         self.handle.stop();
     }
 
+    /// Temporarily pause input grab.
     pub fn pause(&self) {
         self.handle.pause();
     }
 
+    /// Restart input grab.
     pub fn unpause(&self) {
         self.handle.unpause();
     }
 
-    pub fn freeze(&self) -> Handle {
+    /// Whether the [`KeyGrabber`] is running.
+    ///
+    /// Returns true if the `KeyGrabber` has been started but not yet stopped.
+    pub fn running(&self) -> bool {
+        self.handle.running()
+    }
+
+    /// Consume this [`KeyGrabber`] and return only a [`Handle`] to its state.
+    pub fn freeze(self) -> Handle {
         Handle(self.handle.0.clone())
     }
 
+    /// Ignore capture flag on events.
     pub fn ignore_capture(&mut self, ignore_capture: bool) -> &mut Self {
         self.handle.0.lock().unwrap().ignore_capture(ignore_capture);
 
         self
     }
 
+    /// Sets callback to call when a key is pressed.
     pub fn on_key_press<F>(&mut self, on_key_press: F) -> &mut Self
     where
         F: FnMut(&Handle, Modifiers, Keysym, Option<String>) + Send + Sync + 'static,
@@ -84,6 +155,7 @@ impl KeyGrabber {
         self
     }
 
+    /// Sets callback to call when a key is released.
     pub fn on_key_release<F>(&mut self, on_key_release: F) -> &mut Self
     where
         F: FnMut(&Handle, Modifiers, Keysym) + Send + Sync + 'static,
@@ -93,6 +165,7 @@ impl KeyGrabber {
         self
     }
 
+    /// Sets callback to call when some event occurs.
     pub fn on_key_event<F>(&mut self, on_key_event: F) -> &mut Self
     where
         F: FnMut(&Handle, KeyEvent) + Send + Sync + 'static,
@@ -102,6 +175,7 @@ impl KeyGrabber {
         self
     }
 
+    /// Sets callback to call when the [`KeyGrabber`] starts grabbing input.
     pub fn on_start<F>(&mut self, on_start: F) -> &mut Self
     where
         F: Fn(&Handle) + Send + Sync + 'static,
@@ -111,6 +185,7 @@ impl KeyGrabber {
         self
     }
 
+    /// Sets callback to call when the [`KeyGrabber`] stops grabbing input.
     pub fn on_stop<F>(&mut self, on_stop: F) -> &mut Self
     where
         F: Fn(&Handle) + Send + Sync + 'static,
@@ -122,6 +197,7 @@ impl KeyGrabber {
 }
 
 impl Handle {
+    /// Start grabbing input.
     pub fn start(&self) {
         if self.running() {
             return;
@@ -136,7 +212,7 @@ impl Handle {
         );
 
         let Ok(handle) = handle else {
-            //TODO: log error
+            tracing::error!("Could not create a layer to grab inputs.");
             return;
         };
 
@@ -179,6 +255,7 @@ impl Handle {
         self.on_start()
     }
 
+    /// Stop grabbing input.
     pub fn stop(&self) {
         if let Some(handle) = &self.0.lock().unwrap().handle.take() {
             handle.close();
@@ -189,24 +266,33 @@ impl Handle {
         self.on_stop();
     }
 
+    /// Temporarily pause input grabbing.
     pub fn pause(&self) {
-        if let Some(handle) = &self.0.lock().unwrap().handle {
-            let _ = handle.set_keyboard_interactivity(layer::KeyboardInteractivity::None);
-            //TODO: Log Error.
+        if let Some(handle) = &self.0.lock().unwrap().handle
+            && let Err(e) = handle.set_keyboard_interactivity(layer::KeyboardInteractivity::None)
+        {
+            tracing::error!("{e}");
         }
     }
 
+    /// Restart input grabbing.
     pub fn unpause(&self) {
-        if let Some(handle) = &self.0.lock().unwrap().handle {
-            let _ = handle.set_keyboard_interactivity(layer::KeyboardInteractivity::Exclusive);
-            //TODO: Log Error.
+        if let Some(handle) = &self.0.lock().unwrap().handle
+            && let Err(e) =
+                handle.set_keyboard_interactivity(layer::KeyboardInteractivity::Exclusive)
+        {
+            tracing::error!("{e}");
         }
     }
 
+    /// Whether the [`KeyGrabber`] is running.
+    ///
+    /// Returns true if the `KeyGrabber` has been started but not yet stopped.
     pub fn running(&self) -> bool {
         self.0.lock().unwrap().handle.is_some()
     }
 
+    /// Create a non non-owning [`WeakHandle`] referring to the same [`KeyGrabber`] state.
     pub fn downgrade(&self) -> WeakHandle {
         WeakHandle(Arc::downgrade(&self.0))
     }
@@ -227,6 +313,9 @@ impl Handle {
 }
 
 impl WeakHandle {
+    /// Attempts to create a [`Handle`] that refers to the same [`KeyGrabber`].
+    ///
+    /// Returns [`None`] if the [`KeyGrabber`] has already been dropped.
     pub fn upgrade(&self) -> Option<Handle> {
         self.0.upgrade().map(Handle)
     }
