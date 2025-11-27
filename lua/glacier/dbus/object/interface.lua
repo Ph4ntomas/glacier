864d3a12f952df9f19febc91be44e6b2ebcac932
phantomas <phantomas@phantomas.xyz>
2025-11-27 23:49:50 +0100

    lua: glacier.dbus: Implement interface

---
+++ lua/glacier/dbus/object/interface.lua
@@ -0,0 +1,99 @@
+local _errors = require("glacier.dbus.errors")
+local _types = require("glacier.dbus.type")
+local _method = require("glacier.dbus.object.method")
+
+---@class glacier.dbus.object.Interface
+---@field _name glacier.dbus.type.InterfaceName
+---@field _methods table<string, glacier.dbus.object.Method>
+local Interface = {}
+Interface.__index = Interface
+Interface.__name = "dbus.object.Interface"
+
+---@param iface glacier.dbus.object.Interface
+---@return glacier.dbus.object.Interface
+local function Interface_new(iface)
+    return setmetatable(iface, Interface)
+end
+
+---@return glacier.dbus.type.InterfaceName
+function Interface:name()
+    return self._name
+end
+
+---@return string
+function Interface:name_str()
+    return self._name:str()
+end
+
+---@param message glacier.dbus.Message
+---@return glacier.dbus.Message
+function Interface:call(message)
+    local name = message:member():str()
+    local method = self._methods[name]
+
+    if not method then
+        return message:reply_error(_errors.dbus.UnknownMethod, name)
+    end
+
+    return method:call(message)
+end
+
+---@class glacier.dbus.object.interface.Builder
+---@field name glacier.dbus.type.InterfaceName
+---@field methods table<string, glacier.dbus.object.Method>
+local Builder = {}
+Builder.__index = Builder
+Builder.__name = "dbus.object.interface.Builder"
+
+---@param name glacier.dbus.type.InterfaceName
+---@return glacier.dbus.object.interface.Builder?
+local function Builder_new(name)
+    return setmetatable({
+        name = name,
+        methods = {},
+    }, Builder)
+end
+
+---@param method glacier.dbus.object.Method
+function Builder:with_method(method)
+    if not _types.is(method, _method.Method) then
+        return nil, _errors.type.Invalid
+    end
+
+    local name = method:name():str()
+    self.methods[name] = method
+
+    return self
+end
+
+function Builder:build()
+    return Interface_new({
+        _name = self.name,
+        _methods = self.methods,
+    })
+end
+
+local interface = {
+    Interface,
+}
+
+---@param name string|glacier.dbus.type.InterfaceName
+---@return glacier.dbus.object.interface.Builder?
+---@return string?
+function interface.builder(name)
+    if type(name) == "string" then
+        local err
+        ---@diagnostic disable-next-line:cast-local-type
+        name, err = _types.interface_name.try_from_str(name)
+        if not name then
+            return nil, err
+        end
+    elseif not _types.is(name, _types.InterfaceName) then
+        return nil, _errors.type.Invalid
+    end
+
+    ---@cast name glacier.dbus.type.InterfaceName
+    return Builder_new(name), nil
+end
+
+return interface
---
+++ lua/spec/dbus/interface_spec.lua
@@ -0,0 +1,144 @@
+describe("glacier.dbus.object.interface", function()
+    local _errors = require("glacier.dbus.errors")
+    local _types = require("glacier.dbus.type")
+    local _message = require("glacier.dbus.message")
+    local _method = require("glacier.dbus.object.method")
+    local _interface = require("glacier.dbus.object.interface")
+    local _call_res = require("glacier.dbus.message.call_result")
+
+    local interface_name = "org.test.Interface"
+
+    local test_methods
+    local called
+    setup(function()
+        test_methods = {
+            GetName = assert(_method
+                .builder("GetName")
+                :add_output("Name", _types.String)
+                :with_handler(function()
+                    return _types.String(interface_name)
+                end)
+                :build()),
+            Failing = assert(_method
+                .builder("Failing")
+                :with_handler(function()
+                    error(_call_res.CallError.new("org.test.Failing", "FailingMethod"))
+                end)
+                :build()),
+            CallCheck = assert(_method
+                .builder("CallCheck")
+                :with_handler(function()
+                    called = true
+                end)
+                :build()),
+        }
+    end)
+
+    before_each(function()
+        called = false
+    end)
+
+    describe("glacier.dbus.object.interface.Builder", function()
+        it("can build empty interface", function()
+            local builder = assert(_interface.builder("org.test.Interface"))
+            assert(builder:build())
+        end)
+
+        it("accepts InterfaceName", function()
+            assert(_interface.builder(_types.interface_name.from_str("org.test.Interface")))
+        end)
+
+        it("rejects wrong typed name", function()
+            ---@diagnostic disable-next-line:param-type-mismatch
+            local ok, err = _interface.builder(10)
+            assert.falsy(ok)
+            assert.equal(_errors.type.Invalid, err)
+        end)
+
+        it("rejects invalid InterfaceName", function()
+            local ok, err = _interface.builder("org")
+            assert.falsy(ok)
+            assert.equal(_errors.validation.InvalidInterfaceName, err)
+        end)
+
+        it("can add methods", function()
+            local builder = assert(_interface.builder(interface_name))
+            assert(builder:with_method(test_methods.GetName))
+            local iface = assert(builder:build())
+
+            assert.equal(test_methods.GetName, iface._methods["GetName"])
+        end)
+
+        it("rejects wrong type in add_method", function()
+            local builder = assert(_interface.builder(interface_name))
+            ---@diagnostic disable-next-line:param-type-mismatch
+            local ok, err = builder:with_method("not a method")
+
+            assert.falsy(ok)
+            assert.equal(_errors.type.Invalid, err)
+        end)
+    end)
+
+    describe("glacier.dbus.object.Interface", function()
+        local test_message = function(method, body)
+            return assert(_message.method_call("org.test.Dest", "/", interface_name, method, body))
+        end
+
+        ---@type glacier.dbus.object.Interface
+        local test_interface
+        before_each(function()
+            local builder = assert(_interface.builder(interface_name))
+
+            for _, v in pairs(test_methods) do
+                assert(builder:with_method(v))
+            end
+
+            test_interface = assert(builder:build())
+        end)
+
+        it("Dispatch method correctly", function()
+            local msg = test_message("CallCheck")
+
+            test_interface:call(msg)
+
+            assert.truthy(called)
+        end)
+
+        it("forward method return", function()
+            local msg = test_message("GetName")
+
+            local res = test_interface:call(msg)
+
+            assert.equal(_types.message_type.MethodReturn, res:type())
+
+            local expected = _types.Struct({
+                _types.String(interface_name),
+            })
+            assert.same(expected, res.body)
+        end)
+
+        it("forward errors", function()
+            local msg = test_message("Failing")
+
+            local res = test_interface:call(msg)
+
+            assert.equal(_types.message_type.Error, res:type())
+            assert.equal("org.test.Failing", res:error_name())
+        end)
+
+        it("return error on unknown method", function()
+            local method_name = "UndefinedMethod"
+            local msg = test_message(method_name)
+
+            local res = test_interface:call(msg)
+
+            assert.equal(_types.message_type.Error, res:type())
+            assert.equal(_errors.dbus.UnknownMethod, res:error_name())
+
+            local expected = _types.Struct({
+                _types.String(method_name),
+            })
+            assert.same(expected, res.body)
+        end)
+    end)
+end)
