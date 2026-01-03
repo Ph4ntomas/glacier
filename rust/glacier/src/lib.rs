f1042bc2c1c0ab5ccceb1f4976559476d250a990
phantomas <phantomas@phantomas.xyz>
2026-01-03 23:20:55 +0100

    rust: glacier: Extend futures to block on current tokio runtime

--- rust/glacier/src/lib.rs
+++ rust/glacier/src/lib.rs
@@ -23,3 +23,21 @@ pub use modal::modal;
 
 #[doc(inline)]
 pub use bar::Bar;
+
+pub trait BlockOnTokio {
+    type Output;
+
+    fn block_on_tokio(self) -> Self::Output;
+}
+
+impl<F: Future> BlockOnTokio for F {
+    type Output = F::Output;
+
+    /// Blocks on a future using the current Tokio runtime.
+    fn block_on_tokio(self) -> Self::Output {
+        tokio::task::block_in_place(|| {
+            let handle = tokio::runtime::Handle::current();
+            handle.block_on(self)
+        })
+    }
+}
