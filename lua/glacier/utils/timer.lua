79db7a444849ae25130c7721a160ceaafa63def0
phantomas <phantomas@phantomas.xyz>
2026-04-05 23:58:15 +0200

    lua: timer: Migrate timers to snowcap signals.

--- lua/glacier/utils/timer.lua
+++ lua/glacier/utils/timer.lua
@@ -1,7 +1,6 @@
 local cprom = require("cqueues.promise") --[[@as cqueues.promiselib]]
 
 local event_loop = require("glacier.internals.event_loop")
-local signal = require("glacier.signal")
 
 ---glacier.timer module
 ---
@@ -11,15 +10,15 @@ local signal = require("glacier.signal")
 ---@overload fun(...: glacier.timer.Config):glacier.timer.Timer
 local timer = { mt = {} }
 
----Signals emitted by timers.
+---Signals sent by timers
 ---@enum glacier.timer.signals
-timer.signals = {
-    ---Emitted when the timer starts.
-    STARTED = "timer::started",
-    ---Emitted when the timer timeout.
-    TIMEOUT = "timer::timeout",
-    ---Emitted when the timer stop.
-    STOPPED = "timer::stopped",
+local signals = {
+    ---Emitted when a Timer start or re-start.
+    STARTED = "glacier::timer::started",
+    ---Emitted when a Timer stops.
+    STOPPED = "glacier::timer::stopped",
+    ---Emitted when a timeout occurs.
+    TIMEOUT = "glacier::timer::timeout",
 }
 
 ---Cancellable sleep function.
@@ -38,162 +37,184 @@ local function promise_sleep(timeout, promise)
     return true, nil
 end
 
+---@class glacier.timer.Config
+---
+---The signaler to use. If left empty, a new Signaler will be created
+---@field signaler? snowcap.signal.Signaler
+---The repetition interval for this signaler. Defaults to 1.0.
+---@field interval? number
+---Whether this timer should repeat. Defaults to true.
+---@field repeats? boolean
+---Callback to be called on timeout.
+---
+---The callback will be called along with the [`TimerData`], allowing it to manage the current state
+---or emit custom signals in addition to the standard Timeout.
+---@field on_timeout? fun(data: glacier.timer.TimerData)
+
 ---Internal state of a timer.
 ---
----@package
----@class glacier.timer.Inner
----@field signals glacier.signal.Emitter
----@field promise cqueues.promise|nil Promise to control the timer.
+---@class glacier.timer.TimerData
 ---@field interval number Amount of time between events.
----@field once boolean If true, this timer stop itself after timing out.
----@field callback fun()|nil If present, a function to call when the timer timeouts.
+---@field repeats boolean If true, this timer stop itself after timing out.
+---@field signaler snowcap.signal.Signaler
 
 ---A simple timer.
 ---
 ---`Timer`s can be used to defer some actions, or run some callback periodically.
 ---
----When the internal timeout expires, a `Timer` will first run their associated callback, then
----emit `glacier.timer.signals.TIMEOUT` without arguments. If the `Timer` was created with the
----`once` flag, or via `glacier.timer.once()` it will stop after sending the event. Otherwise,
----the `Timer` will wait again in a loop until stopped.
+---When the internal timeout expires, a `Timer` will emit
+---`glacier.timer.signals.TIMEOUT` and call their on_timeout callback if set.
+---If the `Timer` was created without the `repeats` flag set, or via
+---`glacier.timer.once()` it will stop after sending the event. Otherwise, the
+---`Timer` will wait again in a loop until stopped.
 ---
 ---When a timer stop, `glacier.timer.signals.STOPPED` is emitted.
 ---@class glacier.timer.Timer
----@field private inner glacier.timer.Inner Internal state.
+---@field private data glacier.timer.TimerData
+---@field private on_timeout fun(data: glacier.timer.TimerData)|nil If present, a function to call when the timer timeouts.
+---@field private promise cqueues.promise|nil Promise to control the timer.
 local Timer = {}
 
----Start this timer.
+----------------------------
+-- Timer's public methods --
+----------------------------
+
+---Starts the Timer.
 ---
 ---Once started, the timer will periodically emit `timer::timeout`.
 ---
 ---@param now? boolean If true, fire an event before sleep.
 function Timer:start(now)
-    assert(self.inner.promise == nil, "Timer:start() should not be called on a running timer")
+    assert(self.promise == nil, "Timer::start() should not be called on a running timer.")
 
-    self.inner.promise = cprom.new()
+    self.promise = cprom.new()
     event_loop.loop:wrap(function()
-        self:emit(timer.signals.STARTED)
+        self.data.signaler:emit(signals.STARTED, self)
 
         if now then
-            self:emit(timer.signals.TIMEOUT)
+            self:process_timeout()
         end
 
         local continue = true
         while continue do
-            local timeout, restart = promise_sleep(self.inner.interval, self.inner.promise)
+            local timeout, restart = promise_sleep(self.data.interval, self.promise)
 
             if timeout then
-                self:emit(timer.signals.TIMEOUT)
+                self:process_timeout()
             elseif restart then
-                self.inner.promise = restart
+                self.promise = restart
 
-                --No sense in sending a `started` event if we're not looping.
-                if not self.inner.once then
-                    self:emit(timer.signals.STARTED)
+                if self.data.repeats then
+                    self:emit(signals.STARTED)
                 end
             else
                 continue = false
             end
 
-            continue = continue and not self.inner.once
+            continue = continue and self.data.repeats
         end
 
-        self:emit(timer.signals.STOPPED)
+        self:emit(signals.STOPPED)
     end)
 end
 
----Stop this timer
+---Stops the Timer.
 function Timer:stop()
-    if self.inner.promise then
-        self.inner.promise:set(true, nil)
-        self.inner.promise = nil
+    if self.promise then
+        self.promise:set(true, nil)
+        self.promise = nil
     end
 end
 
----Restart this timer
+---Restarts the timer.
 ---
 ---This is equivalent of calling stop if the timer was started, followed by a call to start.
 ---
 ---@param now? boolean If true, fire an event synchronously.
 function Timer:restart(now)
-    if self.inner.promise then
+    if self.promise then
         if now then
-            self:emit(timer.signals.TIMEOUT)
+            self:emit(signals.TIMEOUT)
         end
 
-        self.inner.promise:set(true, cprom.new())
+        self.promise:set(true, cprom.new())
     else
         self:start(now)
     end
 end
 
----Connect to this timer signals
+---Connect to the timer signals.
 ---
----@param name glacier.timer.signals
----@param callback any Callback to call when the signal is emitted.
----@return glacier.signal.SignalHandle # A handle to disconnect this callback.
-function Timer:connect(name, callback)
-    return self.inner.signals:connect(name, callback)
+---This is a shorthand for `Timer:signaler():connect()`.
+---
+---@param sig string
+---@param callback any Callback to call when the signal fire.
+---@return snowcap.signal.SignalHandle # A handle to disconnect this callback.
+function Timer:connect(sig, callback)
+    return self.data.signaler:connect(sig, callback)
 end
 
----Emit a signal.
+---Access the timer `Signaler`.
 ---
----@param name string Signal to emit
----@param ... any Signals parameters
-function Timer:emit(name, ...)
-    return self.inner.signals:emit(name, self, ...)
+---@return snowcap.signal.Signaler
+function Timer:signaler()
+    return self.data.signaler()
 end
 
----Disconnect a signal callback
+-----------------------------
+-- Timer's private methods --
+-----------------------------
+
+---@private
 ---
----@param handle glacier.signal.SignalHandle
-function Timer:disconnect(handle)
-    self.inner.signals:disconnect(handle)
+---Emit standard signals.
+---
+---@param sig string
+function Timer:emit(sig)
+    self.data.signaler:emit(sig, self)
 end
 
----Disconnect all listener.
-function Timer:disconnect_all()
-    self.inner.signals:disconnect_all()
+---@private
+---
+---Function called on timeout.
+function Timer:process_timeout()
+    self:emit(signals.TIMEOUT)
+
+    if self.on_timeout then
+        self.on_timeout(self.data)
+    end
 end
 
----Timer configuration.
----@class (exact) glacier.timer.Config
----@field interval? number Timeout in second between events.
----@field once? boolean If true, the timer will act as a simple delay.
----@field callback? fun() Callback to run on timeout.
+-----------
+-- Other --
+-----------
 
----Create a new timer, according to the configuration.
----
----If `config` is or nil, the default is a timer that will trigger every seconds.
+---Create a new timer.
 ---
 ---@param config glacier.timer.Config
 ---@return glacier.timer.Timer
 function Timer:new(config)
     config = config or {}
+    config.repeats = config.repeats == nil and true or config.repeats
 
     ---@type glacier.timer.Timer
-    local ret = { ---@diagnostic disable-line:redefined-local
-        inner = {
-            signals = signal.emitter(),
+    local ret = {
+        data = {
             interval = config.interval or 1.0,
-            once = config.once,
-            callback = config.callback,
+            repeats = config.repeats,
+            signaler = config.signaler or require("snowcap.signal").Signaler.new(),
         },
+        on_timeout = config.on_timeout,
+        promise = nil,
     }
-
-    setmetatable(ret, self)
-    self.__index = self
-
-    if config.callback then
-        ret:connect(timer.signals.TIMEOUT, config.callback)
-    end
+    ret = setmetatable(ret, { __index = Timer })
 
     return ret
 end
 
----Create a new timer.
+---Create a new timer
 ---
----@param ... glacier.timer.Config Configuration option
+---@param ... glacier.timer.Config
 ---@return glacier.timer.Timer
 function timer.mt:__call(...)
     return Timer:new(...)
@@ -201,16 +222,17 @@ end
 
 ---Create a timer that will fire only once.
 ---
----The timer is started before being returned by this function.
+---The timer is tstarted before being returned by this function.
 ---
----@param callback fun(timer) Function to call when the internal timer expires.
----@param timeout number How long to wait before running `function`
+---@param callback fun(data: glacier.timer.TimerData) Function to call when the timer expires.
+---@param timeout number How long to wait before running the callback.
+---
 ---@return glacier.timer.Timer
 function timer.once(callback, timeout)
     local ret = Timer:new({
         interval = timeout or 0.0,
-        callback = callback,
-        once = true,
+        on_timeout = callback,
+        repeats = false,
     })
 
     ret:start()
@@ -218,9 +240,9 @@ function timer.once(callback, timeout)
     return ret
 end
 
----Create a new timer and start it before returning.
+---Create a new timer and starts it immediately.
 ---
----@param ... glacier.timer.Config Timer configuration option.
+---@param ... glacier.timer.Config Configuration options.
 ---@return glacier.timer.Timer
 function timer.started(...)
     local ret = Timer:new(...)
@@ -231,6 +253,7 @@ function timer.started(...)
 end
 
 timer.Timer = Timer
+timer.signals = signals
 
 ---@diagnostic disable-next-line: param-type-mismatch
 return setmetatable(timer, timer.mt) --[[@as glacier.timer]]
