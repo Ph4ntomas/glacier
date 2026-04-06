1eb819233f995922eed843aee792847350b9ece9
phantomas <phantomas@phantomas.xyz>
2026-04-06 16:00:37 +0200

    lua: w/prompt: Migrate prompt to composable program

--- lua/glacier/widget/prompt.lua
+++ lua/glacier/widget/prompt.lua
@@ -1,193 +1,103 @@
-local Process = require("pinnacle.process")
 local Widget = require("snowcap.widget")
+local StdSig = require("snowcap.widget.signal")
 
-local Base = require("glacier.widget.base")
-local widget_signal = require("glacier.widget.signal")
+local color = require("glacier.misc.color")
 
----Internal prompt module.
-local _prompt = {}
-
----glacier.widget.prompt module.
+---Glacier's Prompts
 ---
 ---@class glacier.widget.prompt
----@field mt metatable This module metatable.
----@overload fun(...:glacier.widget.prompt.Config):glacier.widget.Prompt
+---@field mt metatable The module's metatable
+---
+---@overload fun(...: glacier.widget.prompt.Config): glacier.widget.Prompt
 local prompt = { mt = {} }
 
----Action to execute on an update
----
----@class glacier.widget.prompt.MessageAction
----@field action glacier.widget.prompt.Action
----@field value? string
+----------------------
+-- Type Definitions --
+----------------------
 
----Type of action to execute on an update.
+---Signals emitted by Prompts.
 ---
----@enum glacier.widget.prompt.Action
-_prompt.Action = {
-    INPUT = "prompt::input",
-    SUBMIT = "prompt::submit",
-
-    ---@type fun(value: string): glacier.widget.prompt.MessageAction
-    Input = function(value)
-        return { action = _prompt.Action.INPUT, value = value }
-    end,
-    ---@type fun(): glacier.widget.prompt.MessageAction
-    Submit = function()
-        return { action = _prompt.Action.SUBMIT }
-    end,
+---@enum glacier.widget.prompt.signal
+local _signal = {
+    DONE = "glacier::widget::prompt::done",
 }
 
----A simple prompt to run some commands.
----
----@class glacier.widget.Prompt: glacier.widget.Base
----@field placeholder string The prompt placeholder.
----@field font snowcap.widget.Font Font used to render the text.
----@field padding snowcap.widget.Padding
----@field height snowcap.widget.Length
----@field width snowcap.widget.Length
----@field icon snowcap.widget.text_input.Icon
----@field style snowcap.widget.text_input.Styles
----@field exe_callback fun(command: string) Function to run on submit.
----@field private active boolean Whether this prompt is active
----@field private prompt string What to display at the start of the prompt
-local Prompt = Base:new_class({ type = "Prompt" })
+---Function called when the Prompt is submitted.
+---@alias glacier.widget.prompt.ExeCallback fun(input: string)
 
----Whether the prompt is currently active.
----
----@return boolean
-function Prompt:is_active()
-    return self.active
-end
+---Prompt's events
+---@class glacier.widget.prompt.Event
+---@field activate? {}
+---@field deactivate? {}
+---@field focus? {}
+---@field input? string
+---@field submit? {}
 
----Create the view for this prompt.
----
----If the `Prompt` is not active, nil is return.
----@return snowcap.widget.WidgetDef|nil
-function Prompt:view()
-    if not self.active then
-        return
-    end
+---Prompt's messages.
+---@class glacier.widget.prompt.Message
+---@field id integer
+---@field event glacier.widget.prompt.Event
+local Message = {}
 
-    ---@diagnostic disable-next-line:redefined-local
-    local prompt = Widget.container({
-        height = self.height,
-        width = self.width,
-        valign = Widget.alignment.CENTER,
-        child = Widget.text_input({
-            value = self.content,
-            placeholder = self.placeholder,
-            id = tostring(self),
-            on_input = function(value)
-                return { widget_id = self:id(), action = _prompt.Action.Input(value) }
-            end,
-            on_submit = { widget_id = self:id(), action = _prompt.Action.Submit() },
-            padding = self.padding,
-            font = self.font,
-            icon = self.icon,
-            style = self.style,
-        }),
-    })
+---Prompt's input area appearance
+---@class glacier.widget.prompt.InputStyle
+---Color used to render text.
+---@field fg_color? snowcap.widget.Color
+---Background color used for the prompt
+---@field bg_color? snowcap.widget.Color
+---Border for the Prompt's text_input
+---@field border? snowcap.widget.Border
+---Color used to render the icon
+---@field icon_color? snowcap.widget.Color
+---Color used for the placeholder text
+---@field placeholder_color? snowcap.widget.Color
+---Color used for selected text
+---@field selection_color? snowcap.widget.Color
 
-    return prompt
-end
+---Prompt widget style.
+---@class glacier.widget.prompt.Style: glacier.widget.prompt.InputStyle
+---Font used to render the prompt
+---@field font? snowcap.widget.Font
+---Icon to display on the left or right side of the Prompt
+---@field icon? snowcap.widget.text_input.Icon
+---Prompt's internal padding
+---@field padding? snowcap.widget.Padding
+---Style overrides for active Prompt.
+---@field active? glacier.widget.prompt.InputStyle
+---Style overrides for focused Prompt.
+---@field focused? glacier.widget.prompt.InputStyle
+local Style = {}
 
----Activate this `Prompt`
----
----This function instruct the parent `Layer` to re-render, then to focus this Prompt.
-function Prompt:activate()
-    if self.active then
-        return
-    end
-
-    self.active = true
-    self:refresh()
-    self:focus()
-end
-
----Focus this prompt.
-function Prompt:focus()
-    self:emit(widget_signal.request_focus, tostring(self))
-end
-
----Unfocus this prompt.
-function Prompt:unfocus()
-    self:emit(widget_signal.request_unfocus)
-end
-
----Deactivate this prompt.
----
----When called, this function flush the prompt content, and make it hidden.
-function Prompt:deactivate()
-    self:reset()
-    self.active = false
-
-    self:refresh()
-end
-
----Reset the prompt content.
-function Prompt:reset()
-    self.content = ""
-end
-
----Update this Prompt internal state.
----
----@param msg any
-function Prompt:update(msg)
-    local focusable = require("glacier.widget.operation").focusable
-
-    if not msg then
-        return
-    end
-
-    if msg.operation == focusable.UNFOCUS then
-        self:deactivate()
-    end
-
-    if msg.widget_id == self:id() then
-        ---@diagnostic disable-next-line:redefined-local
-        local msg = msg.action --[[@as glacier.widget.prompt.MessageAction]]
-
-        if msg.action == _prompt.Action.INPUT then
-            self.content = msg.value
-        elseif msg.action == _prompt.Action.SUBMIT then
-            self.exe_callback(self.content)
-
-            self:unfocus()
-        end
-    end
-end
-
----Configuration options for `glacier.widget.prompt.Prompt`.
----
+---Prompt's configuration.
 ---@class glacier.widget.prompt.Config
----@field placeholder? string The prompt placeholder text.
----@field font? snowcap.widget.Font Font to use to render text & placeholder.
----@field icon? snowcap.widget.text_input.Icon|string Icon to display.
----@field style? snowcap.widget.text_input.Style Main style for this prompt.
----@field style_focus? snowcap.widget.text_input.Style Style to use when the prompt is in focus.
----@field padding? snowcap.widget.Padding Internal padding to apply to the input.
----@field height? snowcap.widget.Length Height of the input surrounding container.
----@field width? snowcap.widget.Length Width of the input surrounding container.
----@field exe_callback? fun(input: string) Callback to call when the prompt is submitted.
+---@field placeholder? string
+---@field style? glacier.widget.prompt.Style
+---@field on_exe? glacier.widget.prompt.ExeCallback
 
----Construct a `glacier.widget.Prompt`.
+---Prompt widget.
 ---
----@param config glacier.widget.prompt.Config
----@return glacier.widget.Prompt
-function Prompt:new(config)
-    config = config or {}
+---@class glacier.widget.Prompt: snowcap.widget.Program
+---@field placeholder string
+---@field on_exe glacier.widget.prompt.ExeCallback
+---@field private _style glacier.widget.prompt.Style
+---@field private _content string
+---@field private _id string
+---@field private _active boolean
+local Prompt = setmetatable({}, { __index = require("snowcap.widget.base").Base })
 
-    if type(config.icon) == "string" then
-        local code_point = utf8.codepoint(config.icon --[[@as string]])
-        config.icon = {
-            code_point = code_point,
-        }
-    end
+----------------------
+-- Module functions --
+----------------------
 
-    ---@type glacier.widget.prompt.Config
-    local default_config = {
-        name = "",
-        placeholder = "",
+function prompt.default_view() end
+
+---Default `Prompt` appearance.
+---
+---@param style? glacier.widget.prompt.Style
+---@return glacier.widget.prompt.Style
+function prompt.default_style(style)
+    ---@type glacier.widget.prompt.Style
+    return require("glacier.utils").merge_table({
         font = {
             family = Widget.font.family.Monospace,
             weight = Widget.font.weight.SEMIBOLD,
@@ -196,51 +106,381 @@ function Prompt:new(config)
             code_point = utf8.codepoint(""),
             spacing = 4.0,
         },
-        style = {
-            background = Widget.background.Color(Widget.color.from_rgba(0, 0, 0, 0)),
-            border = { width = 0 },
+        bg_color = color.from_hex("#000000", 0.0),
+        border = {
+            width = 0.0,
         },
-        padding = { top = 0, bottom = 0 },
-        height = Widget.length.Fill,
-        width = Widget.length.Fill,
-        exe_callback = prompt.spawn,
-    }
-
-    ---@diagnostic disable-next-line:redefined-local
-    config = require("glacier.utils").merge_table(default_config, config)
-
-    ---@type glacier.widget.Prompt
-    ---@diagnostic disable-next-line:missing-fields,redefined-local
-    local prompt = Prompt:super({
-        placeholder = config.placeholder,
-        font = config.font,
-        icon = config.icon --[[@as snowcap.widget.text_input.Icon]],
-        padding = config.padding,
-        height = config.height,
-        width = config.width,
-        style = {
-            active = config.style,
-            focused = config.style_focus or config.style,
-            disabled = config.style, -- Sometime, we lose the focus/active state for a few frame.
-        },
-        exe_callback = config.exe_callback,
-        active = false,
-        content = "",
-    })
-
-    return prompt
+        padding = { bottom = 0., left = 0., right = 0., top = 0. },
+    }, style)
 end
 
 ---Default prompt execution callback.
 ---
----@param command string
-function prompt.spawn(command)
-    if command and command ~= "" then
-        Process.spawn(command)
+---This function split the string on whitespace, then call Process::spawn on the resulting
+---array.
+---
+---Spawn a new process.
+---@param input string
+function prompt.spawn(input)
+    if not input or string.len(input) == 0 then
+        return
+    end
+
+    local cmd = {}
+
+    for word in string.gmatch(input, "[^%s]+") do
+        table.insert(cmd, word)
+    end
+
+    if #cmd == 0 then
+        return
+    end
+
+    require("pinnacle.process").spawn(cmd)
+end
+
+-----------------------
+-- Message functions --
+-----------------------
+
+---@package
+---Build a focus message
+---
+---@param id integer
+---@return glacier.widget.prompt.Message
+function Message.focus(id)
+    ---@type glacier.widget.prompt.Message
+    return {
+        id = id,
+        event = {
+            focus = {},
+        },
+    }
+end
+
+---@package
+---Build an activate message
+---
+---@param id integer
+---@return glacier.widget.prompt.Message
+function Message.activate(id)
+    ---@type glacier.widget.prompt.Message
+    return {
+        id = id,
+        event = {
+            activate = {},
+        },
+    }
+end
+
+---@package
+---Build a deactivate message
+---
+---@param id integer
+---@return glacier.widget.prompt.Message
+function Message.deactivate(id)
+    ---@type glacier.widget.prompt.Message
+    return {
+        id = id,
+        event = {
+            deactivate = {},
+        },
+    }
+end
+
+---@package
+---Build an input message.
+---
+---@param id integer
+---@param input string
+---@return glacier.widget.prompt.Message
+function Message.input(id, input)
+    ---@type glacier.widget.prompt.Message
+    return {
+        id = id,
+        event = {
+            input = input,
+        },
+    }
+end
+
+---@package
+---Build a submit message.
+---
+---@param id integer
+---@return glacier.widget.prompt.Message
+function Message.submit(id)
+    ---@type glacier.widget.prompt.Message
+    return {
+        id = id,
+        event = {
+            submit = {},
+        },
+    }
+end
+
+-------------------
+-- Style methods --
+-------------------
+
+---@package
+---
+---@param input_style glacier.widget.prompt.InputStyle
+---@return snowcap.widget.text_input.Style
+local function _to_textinput_style(input_style)
+    ---@type snowcap.widget.text_input.Style
+    return {
+        background = input_style.bg_color and Widget.background.Color(input_style.bg_color) or nil,
+        border = input_style.border,
+        icon = input_style.icon_color,
+        placeholder = input_style.icon_color,
+        selection = input_style.selection_color,
+        value = input_style.fg_color,
+    }
+end
+
+---@private
+function Style:get_active()
+    local deep_copy = require("snowcap.util").deep_copy
+    local merge_table = require("glacier.utils").merge_table
+
+    ---@type glacier.widget.prompt.InputStyle
+    local style = deep_copy({
+        fg_color = self.fg_color,
+        bg_color = self.bg_color,
+        border = self.border,
+        icon_color = self.icon_color,
+        placeholder_color = self.placeholder_color,
+        selection_color = self.selection_color,
+    })
+
+    if self.active then
+        style = merge_table(style, deep_copy(self.active))
+    end
+
+    return _to_textinput_style(style)
+end
+
+---@private
+function Style:get_focused()
+    local deep_copy = require("snowcap.util").deep_copy
+    local merge_table = require("glacier.utils").merge_table
+
+    ---@type glacier.widget.prompt.InputStyle
+    local style = deep_copy({
+        fg_color = self.fg_color,
+        bg_color = self.bg_color,
+        border = self.border,
+        icon_color = self.icon_color,
+        placeholder_color = self.placeholder_color,
+        selection_color = self.selection_color,
+    })
+
+    if self.focused then
+        style = merge_table(style, deep_copy(self.focused))
+    end
+
+    return _to_textinput_style(style or {})
+end
+
+---@package
+---
+---@return snowcap.widget.text_input.Styles
+function Style:to_textinput()
+    ---@type snowcap.widget.text_input.Styles
+    return {
+        active = self:get_active(),
+        focused = self:get_focused(),
+        -- If the UI take too much time to refresh, iced sets the input as disabled, which doesn't look good.
+        disabled = self:get_active(),
+    }
+end
+
+--------------------
+-- Style lifetime --
+--------------------
+
+---@package
+---
+---Initialize a new `Style`
+---@param style glacier.widget.prompt.Style
+---@return glacier.widget.prompt.Style
+function Style.new(style)
+    style = style or {}
+    return setmetatable(style, { __index = Style })
+end
+
+---------------------------
+-- Prompt public methods --
+---------------------------
+
+function Prompt:focus()
+    self:emit(StdSig.send_message, Message.focus(self:id()))
+end
+
+function Prompt:activate()
+    self:emit(StdSig.send_message, Message.activate(self:id()))
+end
+
+function Prompt:deactivate()
+    self:emit(StdSig.send_message, Message.deactivate(self:id()))
+end
+
+----------------------------
+-- Prompt private methods --
+----------------------------
+
+function Prompt:on_focus()
+    if not self._active then
+        return
+    end
+
+    self:emit(StdSig.operation, Widget.operation.focusable.Focus(self._id))
+end
+
+function Prompt:on_activate()
+    if self._active then
+        return
+    end
+
+    self._active = true
+    self:on_focus()
+end
+
+function Prompt:on_deactivate()
+    if not self._active then
+        return
+    end
+
+    self._content = ""
+    self._active = false
+    self:emit(_signal.DONE, tostring(self))
+end
+
+---@private
+---Extract an event from the message.
+---
+---@param msg any|glacier.widget.prompt.Message?
+---@return glacier.widget.prompt.Event?
+function Prompt:get_message_event(msg)
+    if msg and msg.id == self:id() then
+        return msg.event
+    end
+
+    return nil
+end
+
+function Prompt:__tostring()
+    return ("<Prompt#%d>"):format(self:id())
+end
+
+---------------------------------
+-- impl snowcap.widget.Program --
+---------------------------------
+
+---Creates a widget definition for display by Snowcap.
+---
+---A widget may return nil to notify its parent program that it has
+---nothing to display. It's up to the parent to decide whether to display a
+---placeholder or to remove the widget from the tree.
+---@return snowcap.widget.WidgetDef?
+function Prompt:view()
+    if not self._active then
+        return nil
+    end
+
+    local ti = Widget.text_input({
+        id = self._id,
+        placeholder = self.placeholder or "",
+        value = self._content or "",
+        padding = self._style.padding,
+        font = self._style.font,
+        icon = self._style.icon,
+        style = self._style:to_textinput(),
+        on_input = function(input)
+            return Message.input(self:id(), input)
+        end,
+        on_submit = Message.submit(self:id()),
+    })
+
+    return Widget.container({
+        child = ti,
+        height = Widget.length.Fill,
+        width = Widget.length.Fill,
+        valign = Widget.alignment.CENTER,
+    })
+end
+
+---Updates this widget program with the received message.
+---@param msg any|glacier.widget.prompt.Message
+function Prompt:update(msg)
+    local event = self:get_message_event(msg)
+
+    if not event then
+        return
+    end
+
+    if event.activate then
+        self:on_activate()
+    elseif event.deactivate then
+        self:on_deactivate()
+    elseif event.focus then
+        self:on_focus()
+    elseif event.input then
+        self._content = event.input
+    elseif event.submit then
+        if self.on_exe then
+            self.on_exe(self._content)
+        else
+            prompt.spawn(self._content)
+        end
+
+        self:on_deactivate()
     end
 end
 
---- Create a prompt widget which will launch a command when submitted.
+---Called when a surface has been created with this program.
+---
+---A surface handle is provided to allow the program to manupulate
+---the surface. This handle should be passed to any child programs
+---to allow them to use it as well.
+---
+---@param event snowcap.widget.SurfaceEvent
+function Prompt:event(event)
+    if event.focus_lost then
+        self:on_deactivate()
+    end
+end
+
+-----------
+-- Other --
+-----------
+
+---Create a new Prompt widget.
+---
+---@param config glacier.widget.prompt.Config
+---@return glacier.widget.Prompt
+function Prompt:new(config)
+    config = config or {}
+    config.style = require("glacier.utils").merge_table(prompt.default_style(), config.style)
+
+    local base = require("snowcap.widget.base").Base.new()
+    local ret = setmetatable(base, {
+        __index = Prompt,
+        __tostring = Prompt.__tostring,
+    }) --[[@as glacier.widget.Prompt]]
+
+    ret._content = ""
+    ret.placeholder = config.placeholder
+    ret.on_exe = config.on_exe
+    ret._style = Style.new(config.style)
+    ret._id = tostring(ret)
+    ret._active = false
+
+    return ret
+end
+
+---Create a new Prompt widget.
 ---
 ---@param ... glacier.widget.prompt.Config
 ---@return glacier.widget.Prompt
@@ -248,5 +488,8 @@ function prompt.mt:__call(...)
     return Prompt:new(...)
 end
 
----@diagnostic disable-next-line: param-type-mismatch
+prompt.Prompt = Prompt
+prompt.signal = _signal
+
+---@diagnostic disable-next-line:param-type-mismatch
 return setmetatable(prompt, prompt.mt) --[[@as glacier.widget.prompt]]
